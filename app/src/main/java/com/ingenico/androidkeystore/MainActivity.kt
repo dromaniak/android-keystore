f0b065691e4d23f7266d82ef818dc37ed63c73d8
dromaniak <dmitry.romaniak@gmail.com>
2022-05-22 10:12:21 +0300

    - refactored

--- app/src/main/java/com/ingenico/androidkeystore/MainActivity.kt
+++ app/src/main/java/com/ingenico/androidkeystore/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ingenico.androidkeystore
 import android.os.Build
 import android.os.Bundle
 import android.os.Process
+import android.security.KeyPairGeneratorSpec
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.text.method.ScrollingMovementMethod
@@ -11,6 +12,7 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import org.spongycastle.asn1.pkcs.PKCSObjectIdentifiers
 import org.spongycastle.asn1.x500.X500Name
+import org.spongycastle.asn1.x500.style.RFC4519Style.c
 import org.spongycastle.asn1.x509.BasicConstraints
 import org.spongycastle.asn1.x509.Extension
 import org.spongycastle.asn1.x509.ExtensionsGenerator
@@ -19,17 +21,29 @@ import org.spongycastle.operator.jcajce.JcaContentSignerBuilder
 import org.spongycastle.pkcs.PKCS10CertificationRequest
 import org.spongycastle.pkcs.PKCS10CertificationRequestBuilder
 import org.spongycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.FileWriter
 import java.security.KeyPair
 import java.security.KeyPairGenerator
 import java.security.KeyStore
+import android.util.Base64
+import com.ingenico.androidkeystore.MainActivity.Companion.ALGORITHM_RSA
+import com.ingenico.androidkeystore.MainActivity.Companion.KEYSTORE_ANDROID
+import java.math.BigInteger
+import java.security.cert.Certificate
+import java.security.cert.X509Certificate
+import java.security.spec.AlgorithmParameterSpec
 import java.util.*
-import kotlin.experimental.and
+import javax.security.auth.x500.X500Principal
 
 
 @Suppress("DEPRECATION")
 @RequiresApi(Build.VERSION_CODES.M)
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,8 +55,23 @@ class MainActivity : AppCompatActivity() {
         val keyPair = generateKey()
         log(getKey().toString())
 
+        val keyPairCsr = generateCSR(keyPair, "TID SN").encoded
         log("CSR:")
-        log(bytes2HexString(generateCSR(keyPair, "Test").encoded))
+        log(bytes2HexString(keyPairCsr))
+
+        var csr: String? = "-----BEGIN CERTIFICATE REQUEST-----\n"
+        csr += Base64.encodeToString(keyPairCsr, Base64.DEFAULT)
+        csr += "-----END CERTIFICATE REQUEST-----"
+
+        runOnUiThread {
+            val fw = FileWriter(File(dataDir.path + "/keyPub_base64.csr"))
+            fw.write(csr)
+            fw.close()
+
+            val fos = FileOutputStream(File(dataDir.path + "/keyPub.csr"))
+            fos.write(keyPairCsr)
+            fos.close()
+        }
     }
 
     private fun log(s: String) {
@@ -50,10 +79,16 @@ class MainActivity : AppCompatActivity() {
         tv.text = "${tv.text}" + "\n" + s
     }
 
-    fun getKey(): KeyStore.Entry {
-        val ks: KeyStore = KeyStore.getInstance("AndroidKeyStore")
+    private fun getKey(): KeyStore.Entry {
+        val ks: KeyStore = KeyStore.getInstance(KEYSTORE_ANDROID)
         ks.load(null)
-        return ks.getEntry("key", null)
+        return ks.getEntry(RSA_KEY_ALIAS, null)
+    }
+
+    private fun getX509Certificate(alias: String): Certificate {
+        val ks: KeyStore = KeyStore.getInstance(KEYSTORE_ANDROID)
+        ks.load(null)
+        return ks.getCertificate(alias)
     }
 
     private fun generateKey(): KeyPair {
@@ -61,51 +96,51 @@ class MainActivity : AppCompatActivity() {
         val end: Calendar = Calendar.getInstance()
         end.add(Calendar.YEAR, 10)
 
-        val spec = try {
-                KeyGenParameterSpec.Builder(
-                    "key",
+        val spec: AlgorithmParameterSpec = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            KeyGenParameterSpec.Builder(
+                    RSA_KEY_ALIAS,
                     KeyProperties.PURPOSE_SIGN
-                )
-                .setCertificateNotBefore(start.getTime())
-                .setCertificateNotAfter(end.getTime())
-                .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
-                .setDigests(
-                    KeyProperties.DIGEST_SHA256,
-                    KeyProperties.DIGEST_SHA384,
-                    KeyProperties.DIGEST_SHA512
-                )
-                .setKeySize(2048)
-                .build()
-
-//            KeyPairGeneratorSpec.Builder(this@MainActivity)
-//                .setAlias("key")
-//                .setSubject(X500Principal("CN=whatever"))
-//                .setStartDate(start.getTime())
-//                .setEndDate(end.getTime())
-//                .setKeySize(2048)
-//                .setSerialNumber(BigInteger.valueOf(1))
-//                .build()
-        } catch (e: Exception) {
-            throw Error("Cannot make a KeyPairGeneratorSpec", e)
+            )
+                    .setCertificateSubject(X500Principal("${RSA_CERT_SUBJECT_PREFIX}$RSA_KEY_ALIAS"))
+//                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_RSA_PKCS1)
+                    .setSignaturePaddings(KeyProperties.SIGNATURE_PADDING_RSA_PKCS1)
+                    .setDigests(
+                            KeyProperties.DIGEST_SHA256,
+                            KeyProperties.DIGEST_SHA384,
+                            KeyProperties.DIGEST_SHA512
+                    )
+                    .setCertificateSerialNumber(BigInteger.ONE)
+                    .setKeyValidityStart(Date())
+                    .setKeyValidityEnd(end.time)
+                    .setKeySize(RSA_KEY_SIZE)
+                    .build()
+        } else {
+            KeyPairGeneratorSpec.Builder(applicationContext)
+                    .setAlias(RSA_KEY_ALIAS)
+                    .setSubject(X500Principal("${RSA_CERT_SUBJECT_PREFIX}$RSA_KEY_ALIAS"))
+                    .setSerialNumber(BigInteger.ONE)
+                    .setStartDate(Date())
+                    .setEndDate(end.time)
+                    .setKeySize(RSA_KEY_SIZE)
+                    .build()
         }
 
-        val keyPair = try {
+        return try {
             val keyPairGenerator: KeyPairGenerator = KeyPairGenerator.getInstance(
-                "RSA",
-                "AndroidKeyStore"
+                    ALGORITHM_RSA,
+                    KEYSTORE_ANDROID
             )
             keyPairGenerator.initialize(spec)
             keyPairGenerator.generateKeyPair()
         } catch (e: Exception) {
             throw Error("Cannot generate key", e)
         }
-        return keyPair
     }
 
     //Create the certificate signing request (CSR) from private and public keys
 //    @Throws(IOException::class, OperatorCreationException::class)
     fun generateCSR(keyPair: KeyPair, cn: String?): PKCS10CertificationRequest {
-        val CN_PATTERN = "CN=%s, O=Aralink, OU=OrgUnit"
+        val CN_PATTERN = "CN=%s, O=Ingenico, OU=PSA"
         val principal: String = java.lang.String.format(CN_PATTERN, cn)
         val signer: ContentSigner =
             JcaContentSignerBuilder("SHA512WITHRSA").build(keyPair.private)
@@ -141,6 +176,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val EMPTY_STRING = ""
+        private const val EMPTY_STRING = ""
+
+        private const val RSA_KEY_ALIAS = "host_ssl"
+        private const val RSA_KEY_SIZE = 2048
+        private const val RSA_CERT_SUBJECT_PREFIX = "CN="
+//        private const val CERTIFICATE_VALIDITY_DAYS = 360
+        private const val KEYSTORE_ANDROID = "AndroidKeyStore"
+        private const val ALGORITHM_RSA = "RSA"
     }
 }
